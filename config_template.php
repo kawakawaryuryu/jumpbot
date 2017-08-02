2ed0cd7d191588665d759b8e8c69dc4d1618c29a
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2017-08-02 21:11:07 +0900

    add config file

---
+++ config_template.php
@@ -0,0 +1,9 @@
+<?php
+
+// このファイルをコピーして使用する
+// cp config_template.php config.php
+
+define('CONSUMER_KEY', 'xxxxxxxxxxx');
+define('CONSUMER_SECRET', 'xxxxxxxxxxx');
+define('ACCESS_TOKEN', 'xxxxxxxxxxx');
+define('ACCESS_TOKEN_SECRET', 'xxxxxxxxxxx');
