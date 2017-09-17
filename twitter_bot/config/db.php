32b132027bdeb8586bef7d7ad3f322bc8b0e1049
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2017-09-17 15:40:11 +0900

    add env of db

--- twitter_bot/config/db.php
+++ twitter_bot/config/db.php
@@ -3,3 +3,5 @@
 define('USER', getenv('MYSQL_USER'));
 define('PASS', getenv('MYSQL_PASSWORD'));
 define('DATABASE', getenv('MYSQL_DATABASE'));
+define('HOST', getenv('MYSQL_HOST'));
+define('PORT', getenv('MYSQL_PORT'));
