e14102bd3520a61a960848e213be7d745db93766
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2017-10-17 00:03:34 +0900

    config.phpをautoloadできるようにクラス化

---
+++ twitter_bot/config/ConfigTemplate.php
@@ -0,0 +1,13 @@
+<?php
+
+// このファイルをコピーして使用する
+// cp ConfigTemplate.php Config.php
+
+class Config {
+
+    const CONSUMER_KEY = 'xxxxxxxxxxx';
+    const CONSUMER_SECRET = 'xxxxxxxxxxx';
+    const ACCESS_TOKEN = 'xxxxxxxxxxx';
+    const ACCESS_TOKEN_SECRET = 'xxxxxxxxxxx';
+
+}
