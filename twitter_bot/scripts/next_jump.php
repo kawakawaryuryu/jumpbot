8abbc42241d459d2840aef96989f1e13af495ffa
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2017-12-17 22:09:28 +0900

    add script of insertion of next jump info

---
+++ twitter_bot/scripts/next_jump.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once dirname(__FILE__) . '../vendor/autoload.php';
+
+use TwitterBot\models\Jumps;
+
+// insert a next sold jump
+$jumps = new Jumps();
+$releaseDay = date('Y-m-d', mktime(0, 0, 0, 12, 25, 2017));
+$price = 270;
+$combinedIssue = true;
+$jumps->insert($releaseDay, $price, $combinedIssue);
