74d6e3d9f698e9db5d92a9e4305a5372acdedd49
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2017-12-23 00:44:33 +0900

    add a method for getting next active buyer

--- twitter_bot/models/Buyers.php
+++ twitter_bot/models/Buyers.php
@@ -3,6 +3,7 @@
 namespace TwitterBot\models;
 
 use PDO;
+use Exception;
 
 class Buyers extends DBConnection {
 
@@ -12,8 +13,29 @@ class Buyers extends DBConnection {
         $sql = 'select * from ' . Buyers::TABLE
              . ' where deleted_at is null';
 
-        $result = $this->db->query($sql);
+        $sth = $this->db->query($sql);
 
-        return $result->fetchAll(PDO::FETCH_ASSOC);
+        return $sth->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * get next active buyer
+     * @param int $buyerId last buyer
+     * @return array
+     * @throws Exception
+     */
+    public function selectNextActiveBuyer(int $buyerId): array {
+        $activeBuyers = $this->selectActiveBuyers();
+
+        if (count($activeBuyers) == 0) {
+            throw new Exception("No Active Buyers");
+        }
+
+        for ($i = 0; $i < count($activeBuyers); $i++) {
+            if ($activeBuyers[$i]["id"] == $buyerId) {
+                return $activeBuyers[($i + 1) % count($activeBuyers)];
+            }
+        }
+        throw new Exception("buyer_id is invalid");
     }
 }
--- twitter_bot/tests/BuyersTest.php
+++ twitter_bot/tests/BuyersTest.php
@@ -7,6 +7,7 @@ use PHPUnit\DbUnit\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 use TwitterBot\models\Buyers;
 use PDO;
+use Exception;
 
 class BuyersTest extends TestCase {
 
@@ -30,6 +31,17 @@ class BuyersTest extends TestCase {
         return new YamlDataSet(dirname(__FILE__) .'/files/buyers.yml');
     }
 
+    /**
+     * init no active buyers data for select
+     */
+    private function initializeNoActiveBuyersData() {
+        $dataSet = new YamlDataSet(dirname(__FILE__) . '/files/buyers_noactive.yml');
+        $this->databaseTester = null;
+        $this->getDatabaseTester()->setSetUpOperation($this->getSetUpOperation());
+        $this->getDatabaseTester()->setDataSet($dataSet);
+        $this->getDatabaseTester()->onSetUp();
+    }
+
     public function testSelectActiveBuyers() {
         $buyers = new Buyers();
         $actual = $buyers->selectActiveBuyers();
@@ -40,5 +52,34 @@ class BuyersTest extends TestCase {
         // check records
         $this->assertEquals("user", $actual[0]["name"]);
         $this->assertEquals("admin", $actual[1]["name"]);
+    }
+
+    public function testSelectNextActiveBuyer_normal() {
+        $buyers = new Buyers();
+        $actual = $buyers->selectNextActiveBuyer(2);
+
+        // check record
+        $this->assertEquals("user", $actual["name"]);
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage No Active Buyers
+     */
+    public function testSelectNextActiveBuyer_noActive() {
+        // initialize table
+        $this->initializeNoActiveBuyersData();
+
+        $buyers = new Buyers();
+        $buyers->selectNextActiveBuyer(2);
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage buyer_id is invalid
+     */
+    public function testSelectNextActiveBuyer_invalidBuyerId() {
+        $buyers = new Buyers();
+        $buyers->selectNextActiveBuyer(-1);
     }
 }
