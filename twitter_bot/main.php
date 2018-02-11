43b9a36aac000c920ad51dab2b0aa232d8516223
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2018-02-12 01:09:36 +0900

    rename main_by_mysql.php to main.php

---
+++ twitter_bot/main.php
@@ -0,0 +1,20 @@
+<?php
+require dirname(__FILE__) . "/vendor/autoload.php";
+
+use TwitterBot\service\TweetJumpBuyer;
+use TwitterBot\service\NextJump;
+use TwitterBot\service\NextJumpBuyer;
+
+// tweet
+$tweetJumpBuyer = new TweetJumpBuyer();
+$tweetJumpBuyer->tweetNextJumpBuyer();
+
+// update buyer_jump(bought flag = 1)
+
+// insert next jump info
+$nextJump = new NextJump();
+$nextJump->insertNextJump();
+
+// insert next buyer_jump info
+$nextJumpBuyer = new NextJumpBuyer();
+$nextJumpBuyer->insertNextBuyerJump();
--- twitter_bot/main_by_mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-require dirname(__FILE__) . "/vendor/autoload.php";
-
-use TwitterBot\service\TweetJumpBuyer;
-
-$tweetJumpBuyer = new TweetJumpBuyer();
-$tweetJumpBuyer->tweetNextJumpBuyer();
