85bb320661c1a2d969a2ddd1f716b6513b82b1ef
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2018-02-11 23:50:08 +0900

    add test

--- twitter_bot/tests/service/NextJumpBuyerTest.php
+++ twitter_bot/tests/service/NextJumpBuyerTest.php
@@ -12,37 +12,64 @@ class NextJumpBuyerTest extends TestCase {
 
     private $nextJumpBuyer;
 
+    private $buyerJumpMock;
+    private $buyersMock;
+    private $jumpsMock;
+
     public function setUp() {
         $this->nextJumpBuyer = new NextJumpBuyer();
+        $this->buyerJumpMock = $this->createMock(BuyerJump::class);
+        $this->buyersMock = $this->createMock(Buyers::class);
+        $this->jumpsMock = $this->createMock(Jumps::class);
     }
 
-    public function testInsertNextBuyerJump() {
-        // create mock
-        $buyerJumpMock = $this->createMock(BuyerJump::class);
-        $buyerJumpMock->method('selectLastBuyersJumps')->willReturn([
+    public function testInsertNextBuyerJump_normal() {
+        // set mock
+        $this->buyerJumpMock->method('selectLastBuyersJumps')->willReturn([
             "buyer_id" => 1,
             "jump_id" => 1,
             "bought" => 1
         ]);
         // expect to call at once
-        $buyerJumpMock->expects($this->once())->method('insert');
-        $this->nextJumpBuyer->setBuyerJump($buyerJumpMock);
+        $this->buyerJumpMock->expects($this->once())->method('insert');
+        $this->nextJumpBuyer->setBuyerJump($this->buyerJumpMock);
 
-        $buyersMock = $this->createMock(Buyers::class);
-        $buyersMock->method('selectNextActiveBuyer')->willReturn([
+        $this->buyersMock->method('selectNextActiveBuyer')->willReturn([
             "id" => 1,
             "name" => "user"
         ]);
-        $this->nextJumpBuyer->setBuyers($buyersMock);
+        $this->nextJumpBuyer->setBuyers($this->buyersMock);
 
-        $jumpsMock = $this->createMock(Jumps::class);
-        $jumpsMock->method('selectNextJump')->willReturn([
+        $this->jumpsMock->method('selectNextJump')->willReturn([
             "id" => 2,
             "release_day" => "2018-02-11",
             "price" => 260,
             "combined_issue" => 0
         ]);
-        $this->nextJumpBuyer->setJumps($jumpsMock);
+        $this->nextJumpBuyer->setJumps($this->jumpsMock);
+
+        // execute
+        $this->nextJumpBuyer->insertNextBuyerJump();
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage no jumps
+     */
+    public function testInsertNextBuyerJump_nextJumpEmpty() {
+        // set mock
+        $this->buyerJumpMock->method('selectLastBuyersJumps')->willReturn([]);
+        $this->buyerJumpMock->expects($this->never())->method('insert');
+        $this->nextJumpBuyer->setBuyerJump($this->buyerJumpMock);
+
+        $this->buyersMock->method('selectNextActiveBuyer')->willReturn([
+            "id" => 1,
+            "name" => "user"
+        ]);
+        $this->nextJumpBuyer->setBuyers($this->buyersMock);
+
+        $this->jumpsMock->method('selectNextJump')->willReturn([]);
+        $this->nextJumpBuyer->setJumps($this->jumpsMock);
 
         // execute
         $this->nextJumpBuyer->insertNextBuyerJump();
