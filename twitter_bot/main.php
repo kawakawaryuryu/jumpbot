40a9adac342e04f8e72839abab35d8ffb6b95d24
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2018-02-12 16:33:08 +0900

    fix main.php

--- twitter_bot/main.php
+++ twitter_bot/main.php
@@ -10,14 +10,16 @@ use \TwitterBot\service\LastJumpBuyer;
 $tweetJumpBuyer = new TweetJumpBuyer();
 $buyerJumpInfo = $tweetJumpBuyer->tweetNextJumpBuyer();
 
-// update buyer_jump(bought flag = 1)
-$lastJumpBuyer = new LastJumpBuyer();
-$lastJumpBuyer->boughtJump($buyerJumpInfo);
+if (!empty($buyerJumpInfo)) {
+    // update buyer_jump(bought flag = 1)
+    $lastJumpBuyer = new LastJumpBuyer();
+    $lastJumpBuyer->boughtJump($buyerJumpInfo);
 
-// insert next jump info
-$nextJump = new NextJump();
-$nextJump->insertNextJump();
+    // insert next jump info
+    $nextJump = new NextJump();
+    $nextJump->insertNextJump();
 
-// insert next buyer_jump info
-$nextJumpBuyer = new NextJumpBuyer();
-$nextJumpBuyer->insertNextBuyerJump();
+    // insert next buyer_jump info
+    $nextJumpBuyer = new NextJumpBuyer();
+    $nextJumpBuyer->insertNextBuyerJump();
+}
